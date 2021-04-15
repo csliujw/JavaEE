4331f75b40a7d4db58d9bf4ae4a308c701148fa7
csliujw <695466632@qq.com>
2021-04-15 23:33:16 +0800

    leetcode:961 1512 1523

--- concurrent/src/main/java/leetcode/_1523_countOdds.java
+++ concurrent/src/main/java/leetcode/_1523_countOdds.java
@@ -1,6 +1,9 @@
 package leetcode;
 
 public class _1523_countOdds {
+
+    public static void main(String[] args) {
+    }
     public int countOdds1(int low, int high) {
         int count = 0;
         for (int i = low; i <= high; i++) {
