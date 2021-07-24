cc9a9fabd2a82aee3ac911d38cdbf9ea9d5fdca6
csliujw <695466632@qq.com>
2021-07-24 20:01:25 +0800

    io

---
+++ concurrent/src/main/java/thread/_13_read_write_lock/ReadWriteLockDemo.java
@@ -0,0 +1,86 @@
+package thread._13_read_write_lock;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+public class ReadWriteLockDemo {
+    public static void main(String[] args) {
+        MyCache myCache = new MyCache();
+        // 写入
+        for (int i = 1; i <= 5; i++) {
+            final int temp = i;
+            new Thread(() -> {
+                myCache.put(temp + "", temp + "");
+            }, String.valueOf(i)).start();
+        }
+        // 读取
+        for (int i = 1; i <= 5; i++) {
+            final int temp = i;
+            new Thread(() -> {
+                myCache.get(temp + "");
+            }, String.valueOf(i)).start();
+        }
+    }
+}
+
+// 加锁的
+class MyCacheLock {
+    private volatile Map<String, Object> map = new HashMap<>();
+    // 读写锁： 更加细粒度的控制
+    private ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+    private Lock lock = new ReentrantLock();
+
+    // 存，写入的时候，只希望同时只有一个线程写
+    public void put(String key, Object value) {
+        readWriteLock.writeLock().lock();
+        try {
+            System.out.println(Thread.currentThread().getName() + "写入" + key);
+            map.put(key, value);
+            System.out.println(Thread.currentThread().getName() + "写入OK");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            readWriteLock.writeLock().unlock();
+        }
+    }
+
+    // 取，读，所有人都可以读！
+    public void get(String key) {
+        // readLock是为为了防止 写数据
+        readWriteLock.readLock().lock();
+        try {
+            System.out.println(Thread.currentThread().getName() + "读取" + key);
+            Object o = map.get(key);
+            System.out.println(Thread.currentThread().getName() + "读取OK");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            readWriteLock.readLock().unlock();
+        }
+    }
+}
+
+/**
+ * 自定义缓存
+ */
+class MyCache {
+    private volatile Map<String, Object> map = new HashMap<>();
+
+    // 存，写
+    public void put(String key, Object value) {
+        System.out.println(Thread.currentThread().getName() + "写入" + key);
+        map.put(key, value);
+        System.out.println(Thread.currentThread().getName() + "写入OK");
+    }
+
+    // 取，读
+    public void get(String key) {
+        System.out.println(Thread.currentThread().getName() + "读取" + key);
+        Object o = map.get(key);
+        System.out.println(Thread.currentThread().getName() + "读取OK");
+    }
+}
